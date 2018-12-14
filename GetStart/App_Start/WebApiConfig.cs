8de5ca48ed7bedf839b59ef031f80339f95cefd1
jinzhaoyou <wangbaolongcom@163.com>
2018-12-14 10:21:28 +0800

    o

--- GetStart/App_Start/WebApiConfig.cs
+++ GetStart/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -13,10 +14,10 @@ namespace GetStart
 
             // Web API 路由
             config.MapHttpAttributeRoutes();
-
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
---
+++ GetStart/Controllers/ProductsController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using GetStart.Models;
+
+namespace GetStart.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[] {
+            new Product{Id=1,Name="Tomato soup",Category="Groceries",Price=2 },
+            new Product{Id=2,Name="Yo-yo",Category="Toys",Price=3 },
+            new Product{Id=3,Name="Hammer",Category="Hardware",Price=4 }
+        };
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+        public IHttpActionResult GetProduct(int id)
+        {
+            Product result = products.FirstOrDefault(x => x.Id == id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            else {
+                return Ok(result);
+            }
+        }
+        //// GET api/<controller>
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET api/<controller>/5
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+        [HttpGet]
+        public void TestVoid()
+        {
+            
+        }
+        [HttpGet]
+        public HttpResponseMessage TestResponseMessage()
+        {
+            HttpResponseMessage responseMessage = Request.CreateResponse(HttpStatusCode.OK, "value");
+            responseMessage.Content = new StringContent("hello", System.Text.Encoding.Unicode);
+
+
+
+            responseMessage.Headers.CacheControl = new System.Net.Http.Headers.CacheControlHeaderValue()
+            {
+                MaxAge = TimeSpan.FromMinutes(1)
+            };
+            return responseMessage;
+        }
+    }
+}
